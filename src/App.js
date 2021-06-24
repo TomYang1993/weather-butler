e7b5ce86d7145fc285f9e53dc5bb6156362f1034
Tom Yang <xy2286@columbia.edu>
2021-06-23 20:18:01 -0400

    app update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.name = "MyComponent";
 
-class App extends Component {
+    // this.handleClick2 = this.handleClick1.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', (event) => console.log("document",event));
+  }
+
+  handleClick1() {
+    alert(this.name);
+  }
+
+  handleClick3 = (event) => {
+    this.setState({ value: event.target.value })
+    console.log(event)
+    // console.log(this.state.value)
+  }
+
+  handleClick2= (event) => {
+    event.nativeEvent.stopImmediatePropagation();
+    console.log("handle click 2");
+    console.log(event);
+  }
+
   render() {
+    console.log("does it render twice");
     return (
-      <div className="App">
-        <div>
-          <label></label>
-          <input />
-          <button></button>
-        </div>
+      <div>
+        {/* <button onClick={this.handleClick1()}>click 1</button> */}
+        {/* <button onClick={this.handleClick1}>click 2</button> */}
+        <button onClick={this.handleClick2}>click 3</button>
+        <button onClick={this.handleClick3}>click 4</button>
       </div>
     );
   }
 }
-
-export default App;
